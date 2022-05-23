b9a4067a00d00c1c060592c7736d008d5ce8d182
Md-Tur <turibnmostafiz@gmail.com>
2022-05-23 19:23:32 +0600

    drawer icon fixed for mobile view

--- src/Pages/Dashboard/Dashborad.js
+++ src/Pages/Dashboard/Dashborad.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Dashborad = () => {
     return (
         <div className="drawer drawer-mobile">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
-                <h2 className='text-5xl font-bold text center text-cyan-600'>Dashboard</h2>
+            <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content text-center">
+                <h2 className='text-2xl font-bold text center text-cyan-600'>Welcome to Your Dashboard</h2>
                 <Outlet></Outlet>
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
             </div>
             <div className="drawer-side">
-                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+                    <li><Link to='/dashboard'>My Orders</Link></li>
+                    <li><Link to='/dashboard/review'>Add Review</Link></li>
+                    <li><Link to='/dashboard/profile'>My Profile</Link></li>
+                    <li><Link to='/dashboard/addProduct'>Add Product</Link></li>
+                    <li><Link to='/dashboard/manageProducts'>Manage Products</Link></li>
+                    <li><Link to='/dashboard/manageOrders'>Manage Orders</Link></li>
                 </ul>
 
             </div>
