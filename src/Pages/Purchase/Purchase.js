33e5faa578c87cf73bda2a5f93b3d051ee496cd9
Md-Tur <turibnmostafiz@gmail.com>
2022-05-24 23:54:03 +0600

    showing a single data using params

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,46 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Purchase = () => {
+    const [product, setProduct] = useState('');
     const { id } = useParams();
+    console.log(id);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/product/${id}`)
+            .then((res) => res.json())
+            .then((data) => setProduct(data));
+    }, [id]);
 
     return (
-        <div>
-            <h2 className='text-3xl text-center mt-2 mb-2 text-cyan-600'>Confirm Your Purchase{id}</h2>
-            <div className='flex justify-center items-center'>
-                <div>
-                    <div className="card lg:max-w-lg bg-base-100 shadow-xl">
-                        <div className='flex gap-3'>
-                            <div>
-                                <figure className="px-10 pt-10">
-                                    <img className="rounded-xl" style={{ height: "200px", width: "350px" }} src="https://i.ibb.co/8Pv6XVn/Spark-Plug.png" alt="" />
-                                </figure>
-                                <div className="card-body items-center text-center">
-                                    <h2 className="card-title">Product</h2>
-                                    <p><small> Buy From Here</small></p>
-                                    <p><small>Minimum Order: 100 </small></p>
-                                    <p><small> Available: 500 </small></p>
-                                    <p>Price: 300 </p>
-                                    <div className='flex gap-5'>
-                                        <button className="btn btn-error">-</button>
-                                        <input className="input input-bordered" type="text" name="" id="" />
-                                        <button className="btn btn-success">+</button>
-                                    </div>
-                                    <button className="btn btn-outline btn-accent">Confirm</button>
-                                </div>
-                            </div>
-                            <div>
-                                <form>
-                                    <input type="text" id='name' placeholder="Name here" className="block input input-bordered input-sm w-full max-w-xs" />
-                                    <input type="email" id='email' placeholder="Email here" className="block input input-bordered input-sm w-full max-w-xs mb-2 mt-2" />
-                                    <textarea id='review' className="block textarea textarea-bordered" placeholder="Your Review" required></textarea>
-                                    <input id='rating' type="text" placeholder="Your Ratings" className="block input input-bordered input-sm w-full max-w-xs mb-2 mt-2" required />
-                                    <input type="submit" value="Submit Review" className="btn btn-outline btn-success" />
-                                </form>
-                            </div>
+        <div className="grid lg:grid-cols-2 sm:grid-cols-1 lg:px-40 my-16">
+            <div>
+                <div className="card lg:max-w-lg bg-base-100 shadow-xl">
+                    <figure className="px-10 pt-10">
+                        <img className="rounded-xl" style={{ height: "200px", width: "350px" }} src={product.img} alt="" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">{product.name}</h2>
+                        <p><small> {product.description} </small></p>
+                        <p><small>Minimum Order: {product.minimum} </small></p>
+                        <p><small> Available: {product.available} </small></p>
+                        <p>Price: {product.price} </p>
+                        <div className="card-actions">
+
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div>
+                dasdas
             </div>
 
         </div>
